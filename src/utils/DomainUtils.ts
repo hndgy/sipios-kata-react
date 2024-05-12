1dde183b93b0cb6daac14ab8c1d350ed3a06bdee
Nicolas Deguyenne <ndeguyenne@crystal-holding.fr>
2024-05-12 12:22:12 +0200

    refector: fix DomainUtils do not return default value.

--- src/utils/DomainUtils.ts
+++ src/utils/DomainUtils.ts
@@ -1,16 +1,16 @@
 const DOMAIN_PATTERN = /(?<country>(\w)+)_(?<classification>(\w)+)-(?<subClassification>(\w)+)/
 
 const DomainUtils =  {
-    findCountry(domain : string) {
-        return DOMAIN_PATTERN.exec(domain)?.groups?.country ?? domain
+    findCountry(domain : string) : string | undefined {
+        return DOMAIN_PATTERN.exec(domain)?.groups?.country
     },
 
-    findClassification(domain : string) {
-        return DOMAIN_PATTERN.exec(domain)?.groups?.classification ?? domain
+    findClassification(domain : string) : string | undefined {
+        return DOMAIN_PATTERN.exec(domain)?.groups?.classification
     },
 
-    findSubClassification(domain : string) {
-        return DOMAIN_PATTERN.exec(domain)?.groups?.subClassification ?? domain
+    findSubClassification(domain : string) : string | undefined {
+        return DOMAIN_PATTERN.exec(domain)?.groups?.subClassification
     },
 }
 
